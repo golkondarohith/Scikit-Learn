212906fb9a168d52c4533c4c9ab717814a946689
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-21 18:10:59 -0400

    Practice StandardScaler and MinMaxScaler

--- SCIKIT-LEARN PART 1/encoding.py
+++ SCIKIT-LEARN PART 1/encoding.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
 df = pd.read_csv("sample_data.csv")
@@ -7,10 +8,13 @@ df = pd.read_csv("sample_data.csv")
 
 df_label = df.copy()
 
+#Label Encoding
 le = LabelEncoder()
 df_label['Gender_Encoded'] = le.fit_transform(df_label['Gender'])
 df_label['Passed_Encoded'] = le.fit_transform(df_label['Passed'])
 
-
+#One-Hot Encoding
 df_encoded = pd.get_dummies(df_label, columns=['City'])
 print(df_encoded.head())
+
+
---
+++ SCIKIT-LEARN PART 1/feature_scaling.py
@@ -0,0 +1,22 @@
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+#DataFrame
+data = {
+    'StudyHours': [1, 2, 3, 4, 5, 6, 7],
+    'TestScore': [50, 55, 60, 65, 70, 75, 80]
+}
+
+df = pd.DataFrame(data)
+
+#Standard Scaler
+s_scaler = StandardScaler()
+standard_scaler = s_scaler.fit_transform(df)
+print("Standard Scaler Output:\n", standard_scaler)
+
+#MinMax Scaler
+m_scaler = MinMaxScaler()
+minmax_scaler = m_scaler.fit_transform(df)
+print(pd.DataFrame(minmax_scaler, columns=['StudyHours', 'TestScore']))
