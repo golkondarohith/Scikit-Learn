a4dbf643122f44459563bb0d622da27045e230b9
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-25 23:50:10 -0400

    Add MNIST dataset and Train_test_split.

--- SCIKIT-LEARN PART 1/Mini_project.py
+++ SCIKIT-LEARN PART 1/Mini_project.py
@@ -1,3 +1,24 @@
 import pandas as pd
 import numpy as np
+from sklearn.datasets import fetch_openml
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+#Load MNIST Dataset
+mnist = fetch_openml('mnist_784', version=1)
+X, y = mnist.data, mnist.target.astype(int)
+
+#Split into training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
+
+# print(X_train.head())
+# print(X.shape, y.shape)
+
+#Normalize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+print(f"Train Set: {X_train.shape}, Test Set: {X_test.shape}")
+
